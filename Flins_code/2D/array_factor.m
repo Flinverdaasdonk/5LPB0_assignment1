e2b9fd55ebf65fb1dbae77a81544bc21289fa20c
Flin Verdaasdonk <flin.verdaasdonk@gmail.com>
2020-05-03 10:07:41 +0200

    Implemented basic 2D functionality
    Finished implementing basic 2D phased array. Managed to get Figure 6.16 right hand side plot, except for the right scaling (dB).

--- Flins_code/2D/array_factor.m
+++ Flins_code/2D/array_factor.m
@@ -1,11 +1,11 @@
-function S = array_factor(inputArg1,inputArg2)
+function S = array_factor(N, d, a, Theta_0, phi_0, k_0, phi, Theta)
     %ARRAY_FACTOR Takes in antenna parameters, and gives the resulting array
     %factor
     % INPUT
     %    antenna parameters:
     %    N: [N_x, N_y]; number of elements in the x- and y-direction respectively
-    %    a: matrix with dimension N_x*N_y; weights for each phased-array elements
     %    d: [d_x, d_y]; distance between elements in the x- and y-direction respectively
+    %    a: matrix with dimension N_x*N_y; weights for each phased-array elements
     %    Theta_0: scalar; optimum angle of reception in the Theta direction
     %    phi_0: scalar; optimum angle of reception in the phi direction
     %    
@@ -17,9 +17,30 @@ function S = array_factor(inputArg1,inputArg2)
     % OUTPUT:
     %     S: matrix: 2D array factor
 
-    u_0 = sind(Theta_0)*cos(phi_0);
-    v_0 = sind(Theta_0)*sin(phi_0);
-    u = sind(Theta)*cos(phi);
-    v = sind(Theta)*sin(phi);
+    if length(phi) ~= length(Theta)
+        error('phi and Theta should be the same length')
+    end
+    
+    d_x = d(1);
+    d_y = d(2);
+    N_x = N(1);
+    N_y = N(2);
+    
+    u_0 = sind(Theta_0)*cosd(phi_0);
+    v_0 = sind(Theta_0)*sind(phi_0);
+    u = sind(Theta).'*cos(phi);
+    
+    v = sind(Theta).'*sin(phi);
+    
+    S = zeros(size(u));  % initialise a matrix
+    % implementation of (6.40) from "Modern Antennas and Microwave circuits"
+    for u_k = 1:N_x
+       % u_k  % print for time tracking purposes
+       for v_l = 1:N_y
+           pwr = 1i*k_0*((u_k-1)*d_x*(u-u_0) + (v_l-1)*d_y*(v-v_0));
+           S_k_l = abs(a(u_k, v_l))*exp(pwr);
+           S = S + S_k_l;
+       end
+    end
 end
 
--- Flins_code/2D/main.m
+++ Flins_code/2D/main.m
@@ -2,22 +2,37 @@ clear all
 close all
 
 %% Wave specifications
-Theta = linspace(-90,90,10000);  % [degrees] direction of incoming wave from Theta direction
-phi = linspace(-90,90,10000);  % [degrees] direction of incoming wave from phi direction
+Theta = linspace(-90,90,500);  % [degrees] direction of incoming wave from Theta direction
+phi = linspace(-90,90,500);  % [degrees] direction of incoming wave from phi direction
 f = 2.75E9;  %[Hz] operating frequency
 c = 3E8;  %[m/s] speed of light in free space
 lambda = c/f;  % [m] wavelength of the incident wave
 k_0 = 2*pi/lambda;
 
 %% Antenna specifications
-N = [25, 25]; % number of elements in the [x, y] direction
-d_x = 0.525*lambda; %[m], distance between elements on the x-axis
-d_y = 0.525*lambda; %[m], distance between elements on the y-axis
+N_x = 8;  % number of elements in the x-direction
+N_y = 8;  % number of elements in the y-direction 
+N = [N_x, N_y];
+
+d_x = 0.5*lambda; %[m], distance between elements on the x-axis
+d_y = 0.5*lambda; %[m], distance between elements on the y-axis
 d = [d_x, d_y];
 
-Theta_0 = 15;  % [degrees], direction of optimum sensitivity in the Theta direction
-phi_0 = 90;  % [degrees], direction of optimum sensitivity in the phi direction
+Theta_0 = 40;  % [degrees], direction of optimum sensitivity in the Theta direction
+phi_0 = 0;  % [degrees], direction of optimum sensitivity in the phi direction
 
 a = uniform_tapering(N);
+S = array_factor(N, d, a, Theta_0, phi_0, k_0, Theta, phi);
+[NPRP, PRP] = normalised_power_radiation_pattern(S);
 
-% TODO: Finish implementing 6.40, try to get figure 6.16
+% contour(NPRP)
+
+
+
+% plot line (fig. 6.16 right figure)
+mid_v = ceil(length(phi)/2);
+plot(Theta, NPRP(:,mid_v))
+xlim([Theta(1) Theta(end)])
+grid on
+ylabel('Array pattern [dB]')
+xlabel('\Theta [deg]')
