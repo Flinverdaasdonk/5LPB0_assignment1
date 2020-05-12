990f89e0b27c584a59f73676a7484d610a71ef63
Jie226 <64534884+Jie226@users.noreply.github.com>
2020-05-12 19:48:10 +0200

    met chebyshev

--- Sus_code/planar_array.m
+++ Sus_code/planar_array.m
@@ -12,8 +12,9 @@ c = 3*10^8;
 f = 2.75*10^9;     
 dx = 0.525*c/f;      % meters of element spacing in x-direction 
 dy =0.525*c/f;               % meters of element spacing in y-direction 
-th = -90:0.1:90;
-ph = -90:0.1:90;
+th = -90:0.5:90;
+ph = -90:0.5:90;
+[ph,th]=meshgrid(ph,th); 
 th0 = 15;
 ph0 = 90;
 u=sind(th).*cosd(ph);
@@ -32,7 +33,7 @@ n=0;
 n1=0;
 for k=1:25
     beta1=2*pi*(k-1)*0.525*(u-u0);
-    c1=chebwin(25);
+    c1=chebwin(25,32);
     s1=c1(k,1)*exp(1j*beta1);
     s11=exp(1j*beta1);
     m=m+s1;
@@ -40,7 +41,7 @@ for k=1:25
 end
 for l=1:25
     beta2=2*pi*(l-1)*0.525*(v-v0);
-    c2=chebwin(25);
+    c2=chebwin(25,32);
     s2=c2(l,1)*exp(1j*beta2);
     s21=exp(1j*beta2);
     n=n+s2;
@@ -48,9 +49,11 @@ for l=1:25
 end
 s=abs(m.*n).^2/max(abs(m.*n).^2);
 p= 10*log10(s);
-plot(th,p);
-hold on
-ss=abs(m1.*n1).^2/max(abs(m1.*n1).^2);
-pp= 10*log10(ss);
-plot(th,pp);
-axis([-90 90 -100 0]); 
+plot(th,p)%,"--");
+% hold on
+% ss=abs(m1.*n1).^2/max(abs(m1.*n1).^2);
+% pp= 10*log10(ss);
+% plot(th,pp);
+axis([-90 90 -60 0]); grid on
+
+%legend("chebyshev tapering","uniform tapering");
